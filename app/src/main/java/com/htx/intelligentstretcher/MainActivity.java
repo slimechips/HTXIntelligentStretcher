d0e9468e7bde4d7326a9f4cfaf557e449c071036
Simon525 <yj441857342@gmail.com>
2021-06-15 23:16:53 +0800

    oxygen tank regulation

--- app/src/main/java/com/htx/intelligentstretcher/MainActivity.java
+++ app/src/main/java/com/htx/intelligentstretcher/MainActivity.java
@@ -2,17 +2,39 @@ package com.htx.intelligentstretcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 //Test
 public class MainActivity extends AppCompatActivity {
+    private TextView Instant_flow_rate;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView Instant_flow_rate = (TextView) findViewById(R.id.textView3);
-        Instant_flow_rate.setText("40.38");
+        Instant_flow_rate = (TextView) findViewById(R.id.textView2);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Instant_flow_rate.setText("" + i + "SLPM");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
