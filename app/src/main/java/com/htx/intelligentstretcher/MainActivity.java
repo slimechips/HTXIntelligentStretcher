aafca6e6cab66be31f5e944077ea9330c213b936
Simon525 <yj441857342@gmail.com>
2021-06-15 17:43:30 +0800

    TEST

--- app/src/main/java/com/htx/intelligentstretcher/MainActivity.java
+++ app/src/main/java/com/htx/intelligentstretcher/MainActivity.java
@@ -2,6 +2,7 @@ package com.htx.intelligentstretcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 //Test
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView Instant_flow_rate = (TextView) findViewById(R.id.textView3);
+        Instant_flow_rate.setText("40.38");
     }
 }
